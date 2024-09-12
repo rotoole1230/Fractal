443d7e85aa225cbb7e98757010aad0c0e3b55f0c
rotoole1230 <160685353+rotoole1230@users.noreply.github.com>
2024-09-12 14:36:01 -0500

    Project File and Tests

---
+++ FractalUITests/FractalUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FractalUITestsLaunchTests.swift
+//  FractalUITests
+//
+//  Created by Ryan OToole on 9/11/24.
+//
+
+import XCTest
+
+final class FractalUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
